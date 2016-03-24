09c6ee18db426cad22dea3b13b9388371ba4f3e7
Matthias H. Ko <matthiashk@gmail.com>
2016-03-24 17:28:37 -0500

    Created project in Android Studio Setup basic listview, working TODO: change to custom listview using custom array adapter

---
+++ README.md
@@ -0,0 +1,2 @@
+# Capstone Project for Udacity
+
---
+++ RedditReader/app/src/main/java/com/matthiasko/redditreader/MainActivity.java
@@ -0,0 +1,32 @@
+package com.matthiasko.redditreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // create list fragment
+    // load fragment in mainactivity
+
+    /*  change basic list to custom list - add title, name of subreddit, username, time etc
+
+        title = title of post
+        subreddit = name of subreddit
+        username = name of poster
+        source = name of source
+        points = number of points
+        comments = number of comments
+
+        non text items
+        thumbnail preview
+        up/down vote button
+
+     */
+
+}
